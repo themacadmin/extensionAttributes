5f127d375eb14c1aa60515fcfb154efaea1cc4c2
The Mac Admin <miles@themacadmin.com>
2017-01-25 14:20:47 -0800

    Create webExChromeExtensionVersion.bash

---
+++ webExChromeExtensionVersion.bash
@@ -0,0 +1,22 @@
+# Find instances of WebEx Extension
+webExExtensionPath=$( find / -type d -iname jlhmfgmfgeifomenelglieieghnjghma )
+
+## If not present, value is "WebEx Extension Not Installed"
+if [ -z "$webExExtensionPath" ] ; then
+	echo "<result>WebEx Extension Not Installed</result>"
+	exit 0
+fi
+
+# Determine if there is more than one instance of the WebEx Extension
+numberOfWebExExtensions=$(echo $chromeext | awk 'END{print FNR}')
+if [ $numberOfWebExExtensions -gt 1 ] ; then
+		## Determine versions of each
+		## Determine oldest version present
+		## Value is "Multiple instances of WebEx extension present. Oldest is "$oldestWebExExtensionVersion"
+		exit 0
+	else
+		# If only one instance of Extension, value is extension version.
+		
+fi
+
+
